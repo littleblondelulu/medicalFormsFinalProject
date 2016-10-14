02ddba22df9c78312ae8f34ea5d38a232154c71e
lindseyringwald <lindseyringwald@gmail.com>
2016-10-14 14:31:35 -0400

    Fixing a list package error, adding procfile

--- src/main/java/com/theironyard/services/FormRepository.java
+++ src/main/java/com/theironyard/services/FormRepository.java
@@ -1,9 +1,10 @@
 package com.theironyard.services;
 
-import com.sun.tools.javac.util.List;
 import com.theironyard.entities.Form;
 import com.theironyard.entities.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface FormRepository extends CrudRepository<Form, Integer>{
     List<Form> findByName(String title);
