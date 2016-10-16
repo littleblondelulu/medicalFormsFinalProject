96c7b0b258f7f392c3722b40793999111ef30e57
lindseyringwald <lindseyringwald@gmail.com>
2016-10-16 14:53:27 -0400

    added patients and forms to postconstruct

--- src/main/java/com/theironyard/controllers/FinalProjectController.java
+++ src/main/java/com/theironyard/controllers/FinalProjectController.java
@@ -48,25 +48,81 @@ public class FinalProjectController {
 
     @PostConstruct
     public void init() {
+
         if (users.count() == 0) {
             User user = new User();
-            user.setName("Zach");
+            User user1 = new User();
+            User user2 = new User();
+
+            user.setName("Dr. Barringer");
+            user1.setName("Dr. Toboggan");
+            user2.setName("Dr. Sanchez");
+
             users.save(user);
+            users.save(user1);
+            users.save(user2);
+
 
             Patient patient = new Patient();
-            patient.setFirstName("Dee");
+            Patient patient1 = new Patient();
+            Patient patient2 = new Patient();
+            Patient patient3 = new Patient();
+            Patient patient4 = new Patient();
+            Patient patient5 = new Patient();
+            Patient patient6 = new Patient();
+            Patient patient7 = new Patient();
+            Patient patient8 = new Patient();
+            Patient patient9 = new Patient();
+            Patient patient10 = new Patient();
 
-            Form form = new Form();
+            patient.setFirstName("Jeb");
+            patient.setLastName("Gladice");
+            patient1.setFirstName("Allejandro");
+            patient1.setLastName("Rodriguez");
+            patient2.setFirstName("Sandra");
+            patient2.setLastName("Dee");
+            patient3.setFirstName("Sammy");
+            patient3.setLastName("Smith");
+            patient4.setFirstName("Thomas");
+            patient4.setLastName("Edison");
+            patient5.setFirstName("Bill");
+            patient5.setLastName("Gates");
+            patient6.setFirstName("Barack");
+            patient6.setLastName("Obama");
+            patient7.setFirstName("Lloyd");
+            patient7.setLastName("Christmas");
+            patient8.setFirstName("Chip");
+            patient8.setLastName("Jolly");
+            patient9.setFirstName("Suzie");
+            patient9.setLastName("Que");
+            patient10.setFirstName("David");
+            patient10.setLastName("Lee");
 
-            form.setTitle("Oswestry Disability Questionnaire\n");
-            form.setName("testName\n");
-            form.setDescription("This questionnaire has been designed to give us information as to how your back or leg pain is affecting your ability to manage in everyday life. Please answer by checking the scale for the level which best applies to you. \n");
+            Form form1 = new Form();
+            Form form2 = new Form();
+            Form form3 = new Form();
+            Form form4 = new Form();
+
+
+
+            form1.setTitle("Oswestry Disability Questionnaire\n");
+            form2.setTitle("Neck Pain Disability Index\n");
+            form3.setTitle("The Patient-Specific Functional Scale\n");
+            form4.setTitle("PFDI-20\n");
+
+
+            form1.setName("testName\n");
+
+            form1.setDescription("This questionnaire has been designed to give us information as to how your back or leg pain is affecting your ability to manage in everyday life. Please answer by checking the scale for the level which best applies to you. \n");
+
 
             Question question = new Question();
             Question question1 = new Question();
             Question question2 = new Question();
             Question question3 = new Question();
             Question question4 = new Question();
+
+
 
             question.setText("Pain intensity\n");
             question.setAnswer("0(I have no pain at the moment) \n" +
@@ -98,27 +154,117 @@ public class FinalProjectController {
                     "10(Pain prevents me from standing at all)\n");
             questions.save(question4);
 
-            ArrayList<Question> current = new ArrayList<Question>();
-            current.add(question);
-            current.add(question1);
-            current.add(question2);
-            current.add(question3);
-            current.add(question4);
 
-            form.setQuestions(current);
+
+
+            ArrayList<Question> questionGroup1 = new ArrayList<Question>();
+            questionGroup1.add(question);
+            questionGroup1.add(question1);
+            questionGroup1.add(question2);
+            questionGroup1.add(question3);
+            questionGroup1.add(question4);
+
+
+
+//NEW QUESTION LIST
+            Question question5 = new Question();
+            Question question6 = new Question();
+            Question question7 = new Question();
+            Question question8 = new Question();
+            Question question9 = new Question();
+            Question question10 = new Question();
+
+            question5.setText("Personal Care (Washing, Dressing, etc.)");
+            question5.setAnswer("0(I can look after myself normally without causing extra pain)\n" +
+                    "5(I need some help but manage most of my personal care)\n" +
+                    "10(i do not get dressed, I wash with difficulty and stay in bed)\n");
+
+            question6.setText("Reading");
+            question6.setAnswer("0(I can read as much as I want to with no pain in my neck)\n" +
+                    "5(I can’t read as much as I want because of moderate pain in my neck)\n" +
+                    "10(I cannot read at all) \n");
+
+            question7.setText("Headaches\n");
+            question7.setAnswer("0(I have no headaches at all)\n" +
+                    "5(I have moderate headaches, which come frequently)\n" +
+                    "10(I have headaches almost all of the time)\n");
+
+            question8.setText("Work\n");
+            question8.setAnswer("0(I can do as much work as I want to)\n" +
+                    "5(I cannot do my usual work)\n" +
+                    "10(I can’t do any work at all)\n");
+
+            question9.setText("Concentration\n");
+            question9.setAnswer("0(I can concentrate fully when I wast to with no difficulty)\n" +
+                    "5(I have a fair degree of difficulty in concentrating when I want to)\n" +
+                    "10(I cannot concentrate at all)\n");
+
+            question10.setText("Driving\n");
+            question10.setAnswer("0(I can drive my car without any neck pain)\n" +
+                    "5(I can’t drive my car as long as I want because of slight pain in my neck)\n" +
+                    "10(I can’t drive my car at all because of the pain)\n");
+
+
+            questions.save(question5);
+            questions.save(question6);
+            questions.save(question7);
+            questions.save(question8);
+            questions.save(question9);
+            questions.save(question10);
+
+
+            ArrayList<Question> questionGroup2 = new ArrayList<Question>();
+            questionGroup1.add(question5);
+            questionGroup1.add(question6);
+            questionGroup1.add(question7);
+            questionGroup1.add(question8);
+            questionGroup1.add(question9);
+            questionGroup1.add(question10);
+
+            form1.setQuestions(questionGroup1);
+            form2.setQuestions(questionGroup2);
+
+
+
+
 //            try {
 //                user.setPassword(PasswordStorage.createHash("hunter2"));
 //            } catch (PasswordStorage.CannotPerformOperationException e) {
 //                e.printStackTrace();
 //            }
 
-            forms.save(form);
+
+
+            forms.save(form1);
+            forms.save(form2);
+
             patients.save(patient);
+            patients.save(patient1);
+            patients.save(patient2);
+            patients.save(patient3);
+            patients.save(patient4);
+            patients.save(patient5);
+            patients.save(patient6);
+            patients.save(patient7);
+            patients.save(patient8);
+            patients.save(patient9);
+            patients.save(patient10);
 
 
+//QUESTION ABOUT ADDING PATIENTS BY DOCTOR RATHER THAN ALL PATIENTS FOR ALL DOCS
 
             user.getPatients().add(patient);
-            users.save(user);
+            user.getPatients().add(patient1);
+            user.getPatients().add(patient2);
+            user.getPatients().add(patient3);
+            user.getPatients().add(patient4);
+            user.getPatients().add(patient5);
+            user.getPatients().add(patient6);
+            user.getPatients().add(patient7);
+            user.getPatients().add(patient8);
+            user.getPatients().add(patient9);
+            user.getPatients().add(patient10);
+
 
         }
     }
